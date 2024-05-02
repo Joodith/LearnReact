bf8f169c38df99ab94ac8759961c50ff1f1143df
joodith <joodithl@gmail.com>
2024-05-02 14:25:35 +0530

    Code refactoring in Direct Approach of Tic tac tor game

--- DirectApproach/src/App.js
+++ DirectApproach/src/App.js
@@ -18,46 +18,63 @@ function Board({ rows, cols, xIsNext, squares, onPlay, winningConfig }) {
     }
     onPlay(nextSquares);
   }
-  console.log("Board has " + rows + " rows and " + cols + " columns");
 
-  const rowArray = Array.from({ length: rows }, (_, index) => index);
-  const colArray = Array.from({ length: cols }, (_, index) => index);
-  let rowsInBoard = null;
-  if (rows === cols) {
-    rowsInBoard = rowArray.map((rowVal) => {
-      let sqRowNum = "SQR|" + rowVal;
-      let colsInBoard = colArray.map((colVal) => {
-        let curCol = rowVal * cols + colVal;
-        let sqNum = "SQ|" + curCol;
-        return (
-          <Square
-            sqNum={sqNum}
-            value={squares[curCol]}
-            onSquareClick={() => handleClick(curCol)}
-          />
-        );
-      });
-      return (
-        <div key={sqRowNum} className="board-row">
-          {colsInBoard}
-        </div>
-      );
-    });
+  function renderWinnerOrNextPlayerStatus(winner) {
+    let status;
+    if (winner === "X" || winner === "O") {
+      status = "Winner: " + winner;
+    } else if (winner === "=") {
+      status = "Match: Draw";
+    } else {
+      status = "Next Player: " + (xIsNext ? "X" : "O");
+    }
+    return status;
   }
+
+  const renderSquareBoardWithGivenRowsAndCols = () => {
+    const arrayWhoseValuesAreRowIndices = Array.from(
+      { length: rows },
+      (_, index) => index
+    );
+    const arrayWhoseValuesAreColumnIndices = Array.from(
+      { length: cols },
+      (_, index) => index
+    );
+    let boardWithGivenConfig = null;
+    if (rows === cols) {
+      boardWithGivenConfig = arrayWhoseValuesAreRowIndices.map(
+        (rowIndexVal) => {
+          let rowKey = "SQR|" + rowIndexVal;
+          let squaresInCurrentRow = arrayWhoseValuesAreColumnIndices.map(
+            (colIndexVal) => {
+              let currentSquareIndex1D = rowIndexVal * cols + colIndexVal;
+              let currentSquareKey = "SQ|" + currentSquareIndex1D;
+              return (
+                <Square
+                  sqNum={currentSquareKey}
+                  value={squares[currentSquareIndex1D]}
+                  onSquareClick={() => handleClick(currentSquareIndex1D)}
+                />
+              );
+            }
+          );
+          return (
+            <div key={rowKey} className="board-row">
+              {squaresInCurrentRow}
+            </div>
+          );
+        }
+      );
+    }
+    return boardWithGivenConfig;
+  };
 
   const winner = calculateWinner(squares, winningConfig);
-  let status;
-  if (winner === "X" || winner === "O") {
-    status = "Winner: " + winner;
-  } else if (winner === "=") {
-    status = "Match: Draw";
-  } else {
-    status = "Next Player: " + (xIsNext ? "X" : "O");
-  }
+  let status = renderWinnerOrNextPlayerStatus(winner);
   return (
     <>
       <div className="status">{status}</div>
-      {rowsInBoard}
+      {renderSquareBoardWithGivenRowsAndCols}
     </>
   );
 }
@@ -158,60 +175,70 @@ export default function Game() {
 }
 
 function updateWinnerSquares(rows, cols) {
-  let lines = [];
+  let victoryPaths = [];
   updateWinnerRowsAndDiags();
   updateWinnerCols();
 
   function updateWinnerRowsAndDiags() {
-    const leftDiagValues = Array.from({ length: cols }, (_, index) => index);
-    const rightDiagValues = Array.from({ length: cols }, (_, index) => index);
-    for (let r = 0; r < rows; r++) {
-      let curRowValues = Array.from({ length: cols }, (_, index) => index);
-      for (let c = 0; c < cols; c++) {
-        curRowValues[c] = r * cols + c;
-        if (r === c) {
-          leftDiagValues[r] = curRowValues[c];
+    const winningLeftDiagonalIndices = Array.from(
+      { length: cols },
+      (_, index) => index
+    );
+    const winningRightDiagonalIndices = Array.from(
+      { length: cols },
+      (_, index) => index
+    );
+    for (let row = 0; row < rows; row++) {
+      let winningRowIndices = Array.from({ length: cols }, (_, index) => index);
+      for (let col = 0; col < cols; col++) {
+        winningRowIndices[col] = row * cols + col;
+        if (row === col) {
+          winningLeftDiagonalIndices[row] = winningRowIndices[col];
         }
-        if (r + c === rows - 1) {
-          rightDiagValues[r] = curRowValues[c];
+        if (row + col === rows - 1) {
+          winningRightDiagonalIndices[row] = winningRowIndices[col];
         }
       }
-      lines.push(curRowValues);
+      victoryPaths.push(winningRowIndices);
     }
-    lines.push(leftDiagValues);
-    lines.push(rightDiagValues);
+    victoryPaths.push(winningLeftDiagonalIndices);
+    victoryPaths.push(winningRightDiagonalIndices);
   }
 
   function updateWinnerCols() {
-    for (let c = 0; c < cols; c++) {
-      let curColValues = Array.from({ length: rows }, (_, index) => index);
-      for (let r = 0; r < rows; r++) {
-        curColValues[r] = r * cols + c;
+    for (let col = 0; col < cols; col++) {
+      let winningColIndices = Array.from({ length: rows }, (_, index) => index);
+      for (let row = 0; row < rows; row++) {
+        winningColIndices[row] = row * cols + col;
       }
-      lines.push(curColValues);
+      victoryPaths.push(winningColIndices);
     }
   }
-  return lines;
+  return victoryPaths;
 }
 function calculateWinner(squares, winningConfig) {
   let oneNullSquareFound = false;
   for (let i = 0; i < winningConfig.length; i++) {
     let winSquares = winningConfig[i];
-    let sqValue = squares[winSquares[0]];
-    let winValue = null;
-    for (let sq = 1; sq < winSquares.length; sq++) {
-      let sqIndex = winSquares[sq];
-      if (squares[sqIndex] === null || sqValue === null) {
+    let valueInSquare = squares[winSquares[0]];
+    let winnerSymbol = null;
+    for (let i = 1; i < winSquares.length; i++) {
+      let squareIndex = winSquares[i];
+      if (squares[squareIndex] === null || valueInSquare === null) {
         oneNullSquareFound = true;
       }
-      if (squares[sqIndex] && sqValue && squares[sqIndex] === sqValue) {
-        winValue = sqValue;
+      if (
+        squares[squareIndex] &&
+        valueInSquare &&
+        squares[squareIndex] === valueInSquare
+      ) {
+        winnerSymbol = valueInSquare;
       } else {
-        winValue = null;
+        winnerSymbol = null;
         break;
       }
     }
-    if (winValue) return winValue;
+    if (winnerSymbol) return winnerSymbol;
   }
   if (!oneNullSquareFound) return "=";
   return null;
